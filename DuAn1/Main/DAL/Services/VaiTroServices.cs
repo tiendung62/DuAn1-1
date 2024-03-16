08c320035dc8646d9d8b0a3a1ebcd342fe8f0a71
khai520 <khai5108@gmail.com>
2024-03-16 18:09:37 +0700

    new services

--- DuAn1/Main/DAL/Services/VaiTroServices.cs
+++ DuAn1/Main/DAL/Services/VaiTroServices.cs
@@ -1,20 +1,19 @@
-﻿using System;
+﻿using Main.BLL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WinFormsApp1.Models;
 using WinFormsApp1.Repositories;
 
 namespace WinFormsApp1.Services
 {
     internal class VaiTroServices
     {
-        VaiTroRepo vtrepo = new VaiTroRepo();
 
-        public List<VaiTro> GetVaiTros()
+        public List<VaiTro> GetVaiTros(List<VaiTro> list)
         {
-            return vtrepo.getallVaiTro().ToList();
+            return list;
         }
     }
 }
