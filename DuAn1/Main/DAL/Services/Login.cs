42170ac0d62c58ca1674d92f8a9cc765d1aac8c8
khai520 <khai5108@gmail.com>
2024-03-16 17:14:01 +0700

    new Code

---
+++ DuAn1/Main/DAL/Services/Login.cs
@@ -0,0 +1,27 @@
+﻿using Main.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.DAL.Services
+{
+    internal class Login
+    {
+        public List<NguoiDung> getAll(List<NguoiDung> list)
+        {
+            return list.ToList();
+        }
+        public bool CheckLogin(string tk , string mk , List<NguoiDung> list)
+        {
+            if (getAll(list).Where(x => x.Email == tk && x.MatKhau == mk).ToList().Count() != 0)
+            {
+                return true;
+            } else 
+            {
+                return false; 
+            }
+        }
+    }
+}
